e12cfad5f81bf8274e5516aff447034e9ae360dc
seankrummel <sean.krummel@gmail.com>
2018-12-03 15:13:42 -0600

    updated post /users endpoint to use hashPassword method

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,21 @@ router.post('/', (req, res, next) => {
     }
   });
   const {fullname, username, password} = req.body;
-  const newUser = {fullname, username, password};
+  const newUser = {fullname, username};
 
-  User.create(newUser)
+  return User.hashPassword(password)
+    .then(digest => {
+      newUser.password = digest;
+      return User.create(newUser);
+    })
     .then(result => res.location(`${req.originalUrl}/${result.id}`).status(201).json(result))
-    .catch(err => next(err));
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
 module.exports = router;
