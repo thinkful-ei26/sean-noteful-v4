23fe9ebc6dcfb5717f8eb3ad85b10f768e4942d0
seankrummel <sean.krummel@gmail.com>
2018-12-04 11:11:11 -0600

    added JWT to /routes/auth.js router

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const localAuth = passport.authenticate('local', {session: false, failWithError: true});
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
 
-router.post('/login', localAuth, (req, res) => res.json(req.user));
+function createAuthToken(user) {
+  return jwt.sign({user}, JWT_SECRET, {subject: user.username, expiresIn: JWT_EXPIRY});
+}
+
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
+});
 
 module.exports = router;
